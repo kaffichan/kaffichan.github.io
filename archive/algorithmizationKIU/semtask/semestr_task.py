0242ffba467ff55b6401b8b25dde39f7eeb3c59f
kaffi <63959513+kaffichan@users.noreply.github.com>
2025-12-19 12:16:18 +0300

    upd2

--- archive/algorithmizationKIU/semtask/semestr_task.py
+++ archive/algorithmizationKIU/semtask/semestr_task.py
@@ -1,5 +1,4 @@
-def _validate_input(data):
-
+def _valid_input(data):
     # проверка является ли вход списком
     if not isinstance(data, list):
         return False
@@ -15,66 +14,53 @@ def _validate_input(data):
     return True
 
 def summa(data):
-    if not _validate_input(data):
+    if not _valid_input(data):
         return "error"
     
-    try:
-        # присваиваем элементы списка переменным
-        a, b, c, d = data
+    # присваиваем элементы списка переменным
+    a, b, c, d = data
         
-        # вычисление формулы (a + bi) + (c + di) = (a + c) + (b + d)i
-        return [float(a + c), float(b + d)]
-    except Exception:
-        # если возникла ошибка, возвращаем строку "error"
-        return "error"
+    # вычисление формулы (a + bi) + (c + di) = (a + c) + (b + d)i
+    return [float(a + c), float(b + d)]
 
 def raznost(data):
-    if not _validate_input(data):
+    if not _valid_input(data):
         return "error"
     
-    try:
-        a, b, c, d = data
+    a, b, c, d = data
         
-        # вычисление формулы (a + bi) - (c + di) = (a - c) + (b - d)i
-        return [float(a - c), float(b - d)]
-    except Exception:
-        return "error"
+    # вычисление формулы (a + bi) - (c + di) = (a - c) + (b - d)i
+    return [float(a - c), float(b - d)]
 
 def multi(data):
-    if not _validate_input(data):
+    if not _valid_input(data):
         return "error"
     
-    try:
-        a, b, c, d = data
+    a, b, c, d = data
         
-        # происходит вычисление действительного и мнимого числа по формуле
-        # (a + bi) * (c + di) = (ac - bd) + (ad + bc)i
-        real_mult = (a * c) - (b * d)
-        imag_mult = (a * d) + (b * c)
+    # происходит вычисление действительного и мнимого числа по формуле
+    # (a + bi) * (c + di) = (ac - bd) + (ad + bc)i
+    real_mult = (a * c) - (b * d)
+    imag_mult = (a * d) + (b * c)
         
-        return [float(real_mult), float(imag_mult)]
-    except Exception:
-        return "error"
+    return [float(real_mult), float(imag_mult)]
 
 def divide(data):
-    if not _validate_input(data):
+    if not _valid_input(data):
         return "error"
     
-    try:
-        a, b, c, d = data
+    a, b, c, d = data
         
-        # делитель
-        denominator = (c**2) + (d**2)
+    # делитель
+    denominator = (c**2) + (d**2)
         
-        # на 0 делить нельзя
-        if denominator == 0:
-            return "error"
-        
-        # вычисление деления по формуле
-        # (a + bi) / (c + di)
-        real_div = ((a * c) + (b * d)) / denominator
-        imag_div = ((b * c) - (a * d)) / denominator
-        
-        return [round(real_div, 4), round(imag_div, 4)]
-    except Exception:
+    # на 0 делить нельзя
+    if denominator == 0:
         return "error"
+        
+    # вычисление деления по формуле
+    # (a + bi) / (c + di)
+    real_div = ((a * c) + (b * d)) / denominator
+    imag_div = ((b * c) - (a * d)) / denominator
+        
+    return [round(real_div, 4), round(imag_div, 4)]
